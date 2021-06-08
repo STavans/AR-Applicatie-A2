2c39f29117a3ea37d7cea6bef7d7e080cd19f317
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2021-06-08 14:29:59 +0200

    [ADD] gamelogic classes

---
+++ GameLogic.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Coordinate.h"
+#include "HitSphere.h"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "tigl.h"
+#include "FpsCam.h"
+#include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+
+class GameLogic
+{
+	Coordinate* cArray;
+
+};
+
